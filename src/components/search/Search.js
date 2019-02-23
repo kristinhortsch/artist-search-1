76b1db0be7816f58b89c16788f5278518e02b160
Dyanna Tolman <dyannast@gmail.com>
2019-02-22 16:52:08 -0800

    styling

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,6 +2,8 @@ import React, { PureComponent, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { getArtists } from '../../services/artistsAndWorks';
 import Artist from '../artist/Artist';
+import styles from '../App.css';
+import 'normalize-css';
 
 class Search extends PureComponent {
   state = {
@@ -21,16 +23,15 @@ class Search extends PureComponent {
     this.setState(state => ({ offset: state.offset - 10 }));
   }
 
+  handleClick = () => {
+    const { artist, offset } = this.state;
+    getArtists(artist, offset)
+      .then(response => {
+        this.setState({ count: response.count });
+        this.setState({ artists: response.results });
+      });
+  };
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.artist !== this.state.artist) {
-      const { artist, offset } = this.state;
-      getArtists(artist, offset)
-        .then(response => {
-          this.setState({ count: response.count });
-          this.setState({ artists: response.results });
-        });
-    }
-
     if(prevState.offset !== this.state.offset) {
       const { artist, offset } = this.state;
       getArtists(artist, offset)
@@ -49,22 +50,25 @@ class Search extends PureComponent {
     const totalPages = Math.ceil(this.state.count / 10);
     const { artist } = this.state;
     const listOfArtists = this.state.artists.map(artist => {
-      return <Link to={`/artist/${artist.name}/${artist.id}`} key={artist.id}><li><Artist artist={artist}/></li></Link> ;
+      return <Link className={styles.link} to={`/artist/${artist.name}/${artist.id}`} key={artist.id}><li><Artist artist={artist}/></li></Link> ;
     });
     
     return (
       <Fragment>
-        <h1>Search</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="artist" value={artist} onChange={this.handleSubmit}></input>
-        </form>
+        <div className={styles.header}>
+          <h1>Search</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="artist" value={artist} onChange={this.handleSubmit}></input>
+            <button onClick={this.handleClick}>Search</button>
+          </form>
+        </div>
         <div>
-          <h3>Artists</h3>
-          <div>
+          <div className={styles.paging}>
             <button onClick={this.decrement}>Back</button>
             {this.state.artist && <p>{this.state.page} / {totalPages}</p>}
             <button onClick={this.increment}>Forward</button>
           </div>
+          <h3>Artists</h3>
           <ul>
             {listOfArtists}
           </ul>
