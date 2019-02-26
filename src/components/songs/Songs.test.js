a86f086853711087121c6a929f3c4d3ccb11c569
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-25 16:06:27 -0800

    Songs test is passing, added in memory router

--- src/components/songs/Songs.test.js
+++ src/components/songs/Songs.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Search  from './Search';
+import Songs  from './Songs';
+import { MemoryRouter } from 'react-router-dom';
 
 jest.mock('../../services/artistsAndWorks.js');
 
-describe('Search', () => {
+describe('Songs', () => {
   it('matches a snapshot', () => {
+    const match = {
+      params: {
+        id: '2344'
+      }
+    };
     const tree = renderer.create(
-      <Search />
+      <MemoryRouter><Songs match={match}/></MemoryRouter>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
