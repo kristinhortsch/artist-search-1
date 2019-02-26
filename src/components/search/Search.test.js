70a85c9fe5daaee61cd019d1f5866ec70f50a63d
cruzayye <johnnyc010392@gmail.com>
2019-02-25 16:37:13 -0800

    button test passes aka first enzyme test

--- src/components/search/Search.test.js
+++ src/components/search/Search.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Search  from './Search';
+import { shallow } from 'enzyme';
+// import  sinon  from 'sinon';
+
 
 jest.mock('../../services/artistsAndWorks.js');
 
@@ -10,5 +13,15 @@ describe('Search', () => {
       <Search />
     ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('tests buttons', () => {
+    const onClick = jest.fn();
+    onClick();
+    onClick();
+    const wrapper = shallow(<Search onClick={onClick} />);
+    wrapper.find('button').at(0).simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(2);
+
   });
 });
